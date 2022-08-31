e4e5a37f28c24f3199a38159eab9c8b1b0733f6d
laurasusano22 <lauritasusano.10@gmail.com>
2022-08-31 11:36:20 -0500

    Video 34

--- js/app.js
+++ js/app.js
@@ -2,27 +2,47 @@
 
 let ataqueJugador 
 let ataqueEnemigo
+let vidasJugador = 3
+let vidasEnemigo = 3
 
 // Iniciar juego
-
 function iniciarJuego(){
+    let sectionSeleccionarAtaque = document.getElementById('seleccionar-ataque')
+    // No aparezca al inicio del juego
+    sectionSeleccionarAtaque.style.display = 'none'
+
+    // No aparezca al inicio del juego
+    let sectionReiniciar = document.getElementById('boton-reiniciar')
+    sectionReiniciar.style.display = 'none'
+
     // Variables
     let botonMascotaJugador = document.getElementById('boton-mascota')
     botonMascotaJugador.addEventListener('click',seleccionarMascotaJugador)
 
     // Eventos
     let botonFuego = document.getElementById('boton-fuego')
+    //Escuchador de eventos
     botonFuego.addEventListener('click', ataqueFuego)
     let botonAgua = document.getElementById('boton-agua')
     botonAgua.addEventListener('click', ataqueAgua)
     let botonTierra = document.getElementById('boton-tierra')
     botonTierra.addEventListener('click', ataqueTierra)
 
+    // Reiniciar juego
+    let botonReiniciar = document.getElementById('boton-reiniciar')
+    botonReiniciar.addEventListener('click', reiniciarJuego)
 }
 
 // Funciones para mascotas
-
 function seleccionarMascotaJugador() {
+    // No aparezca al inicio del juego
+    let sectionSeleccionarMascota = document.getElementById('seleccionar-mascota')
+    sectionSeleccionarMascota.style.display = 'none'
+    
+    // Si aparezca al inicio del juego
+    let sectionSeleccionarAtaque = document.getElementById('seleccionar-ataque')
+    sectionSeleccionarAtaque.style.display = 'block'
+
     let inputHipodoge = document.getElementById('hipodoge')
     let inputCapipepo = document.getElementById('capipepo')
     let inputRatigueya = document.getElementById('ratigueya')
@@ -49,7 +69,6 @@ function seleccionarMascotaJugador() {
 
     seleccionarMascotaEnemigo()
 }
-
 function seleccionarMascotaEnemigo () {
     let mascotaAleatoria = aleatorio(1,6)
     let spanMascotaEnemigo = document.getElementById('mascota-enemigo')
@@ -76,22 +95,18 @@ function seleccionarMascotaEnemigo () {
 }
 
 // Funciones para ataques
-
 function ataqueFuego() {
     ataqueJugador = 'FUEGO'
     ataqueAleatorioEnemigo()
 }
-
 function ataqueAgua() {
     ataqueJugador = 'AGUA'
     ataqueAleatorioEnemigo()
 }
-
 function ataqueTierra() {
     ataqueJugador = 'TIERRA'
     ataqueAleatorioEnemigo()
 }
-
 function ataqueAleatorioEnemigo() {
     let ataqueAleatorio = aleatorio(1,3)
     
@@ -108,18 +123,35 @@ function ataqueAleatorioEnemigo() {
 
 // Funcion para el combate
 function combate() {
+    let spanVidasJugador = document.getElementById('vidas-jugador')
+    let spanVidasEnemigo = document.getElementById('vidas-enemigo')
+
     if(ataqueJugador==ataqueEnemigo){
     crearMensaje("EMPATE")
     }
     else if((ataqueJugador == 'FUEGO') && (ataqueEnemigo == 'TIERRA') ||(ataqueJugador == 'AGUA' && ataqueEnemigo == 'FUEGO') || ataqueJugador == 'TIERRA' && ataqueEnemigo == 'AGUA'){
         crearMensaje("GANASTE")
+        vidasEnemigo--
+        spanVidasEnemigo.innerHTML = vidasEnemigo
     } else{
         crearMensaje("PERDISTE")
+        vidasJugador--
+        spanVidasJugador.innerHTML = vidasJugador
+    }
+
+    revisarVidas()
+}
+
+// Funcion para revisar vidas
+function revisarVidas(){
+    if (vidasEnemigo == 0){
+        crearMensajeFinal("FELICITACIONES! Ganaste :)")
+    } else if (vidasJugador == 0){
+        crearMensajeFinal("Lo siento, perdiste :(")
     }
 }
 
-// Funciones para mensajes
-
+// Funciones para mensajes, se crea un mensaje nuevo para cada ataque
 function crearMensaje(resultado){
     let sectionMensajes = document.getElementById('mensajes')
 
@@ -130,10 +162,37 @@ function crearMensaje(resultado){
     sectionMensajes.appendChild(parrafo)
 }
 
+// Se crea un Mensaje para el ganador
+function crearMensajeFinal(resultadoFinal){
+    let sectionMensajes = document.getElementById('mensajes')
+
+    let parrafo = document.createElement('p')
+    parrafo.innerHTML = resultadoFinal
+
+    sectionMensajes.appendChild(parrafo)
+
+    let botonFuego = document.getElementById('boton-fuego')
+    botonFuego.disabled = true
+    let botonAgua = document.getElementById('boton-agua')
+    botonAgua.disabled = true
+    let botonTierra = document.getElementById('boton-tierra')
+    botonTierra.disabled = true
+
+    let sectionReiniciar = document.getElementById('boton-reiniciar')
+    sectionReiniciar.style.display = 'block'
+}
+
+// Funciones para aleatoridad
 function aleatorio(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+// Reiniciar juego
+function reiniciarJuego() {
+    location.reload()
 }
 
 // NOTA: esta es otra manera de llamar al script despues de que se cargue todo el HTML. La funcion iniciarJuego se carga cuando ya todo el contenido esta cargado.
 
 window.addEventListener('load', iniciarJuego)
+
